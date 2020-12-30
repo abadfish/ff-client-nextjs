127ed2d381ee24ab75a2d056d770707ee3930081
abadfish <kirsten.ofarrell@gmail.com>
2020-12-29 21:49:39 -0500

    messing around with next

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,18 @@
+import { useState } from 'react'
 import Layout from '../components/Layout'
 import styled from 'styled-components'
 import { Page, Content } from './index'
 import HabitList from '../components/HabitList'
+import HabitForm from '../components/HabitForm'
 
 const About = () => {
+  const [habits, setHabits ] = useState(['Do the dishes'])
     return (
         <Layout>
           <Page>
             <Content>
-              
+              <HabitForm setHabits={ setHabits }/>
+              <HabitList habits={ habits }/>
             </Content>
           </Page>
 
