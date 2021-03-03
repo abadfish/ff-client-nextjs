97fea7ab9495d2859ff7e88fba65dfe1ecb8d7b7
abadfish <kirsten.ofarrell@gmail.com>
2021-03-02 19:22:15 -0500

    added contact info to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -4,13 +4,21 @@ import Layout from '../components/Layout'
 import Input from 'muicss/lib/react/input';
 import Button from 'muicss/lib/react/button';
 
-
+// Farriers Fix Inc.
+// PO Box 191
+// Bedford, NY 10506
+//
+// For More Information you can Call (888) 779-7775,
+// or leave us a message below.
 const Contact = () => {
   return (
     <Layout>
       <ContactForm>
         <form action="">
           <Input type="text"/>
+          <Input type="text"/>
+          <Input type="text"/>
+
           <Button>Submit</Button>
         </form>
       </ContactForm>
